b2ba6d089ee7393a2c5c512d07de8c60663b23a4
Jacques Toquard <jacques.toquard@proton.me>
2024-11-06 18:37:01 +0100

    2024-11-06T17:37:01Z

--- FrontEnd/src/js/modal.js
+++ FrontEnd/src/js/modal.js
@@ -1,15 +1,8 @@
 import { galleryService } from "./gallery-service.js";
 
 class Modal {
-
-    /**
-     * Modal constructor
-     * @constructor
-     * Sets up the modal element and its associated events.
-     * @param {HTMLElement} modalElement The HTML element that will be used as the modal.
-     * @param {HTMLElement} modalClose The HTML element that will be used to close the modal.
-     */
     constructor() {
+        this.addWorkMode = false;
         this.modalElement = document.getElementById("modal");
         const modalClose = document.getElementById("modal-close");
         modalClose.addEventListener("click", (event) => {
@@ -21,6 +14,14 @@ class Modal {
             }
         });
         galleryService.renderModalGallery();
+        this.addWorkModeButton = document.getElementById("modal-add-photo");
+        this.addWorkModeButton.addEventListener("click", (event) => {
+            this.addWorkModeOpen();
+        });
+        this.modalTitle = document.getElementById("modal-title");
+        this.modalGallery = document.getElementById("modal-gallery");
+        this.modalWorkModeDiv = document.getElementById("modal-work-mode");
+        this.modalWorkModeValider = document.getElementById("modal-valider");//TODO add event listener
     }
 
     /**
@@ -35,6 +36,27 @@ class Modal {
      */
     close() {
         this.modalElement.style.display = "none";
+        if (this.addWorkMode) {
+            this.addWorkModeClose();
+        }
+    }
+
+    addWorkModeOpen() {
+        this.addWorkMode = true;
+        this.modalTitle.textContent = "Ajout photo";
+        this.modalGallery.style.display = "none";
+        this.addWorkModeButton.style.display = "none";
+        this.modalWorkModeValider.style.display = "block";
+        this.modalWorkModeDiv.style.display = "flex";
+    }
+
+    addWorkModeClose() {
+        this.addWorkMode = false;
+        this.modalTitle.textContent = "Galerie photo";
+        this.modalGallery.style.display = "grid";
+        this.addWorkModeButton.style.display = "block";
+        this.modalWorkModeValider.style.display = "none";
+        this.modalWorkModeDiv.style.display = "none";
     }
 }
 
